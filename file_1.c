25326d46b84af7384b5cd62c821c4442f8eb324b
Wims2020 <nsmamatha60@gmail.com>
2024-04-28 12:50:41 +0000

    conflict resolved

---
+++ file_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+	int number1, number 2, sum:
+
+	printf("Enter two intergers: ");
+	scanf("%d %d", &number1, &number2);
+
+	sum = number1 + number2;
+
+	printf("%d + %d", number1, number2, sum);
+	return 0;
+}
+<<<<<<< HEAD
+
+int addfunc ( int a, int b)
+{
+	int c = a+b;
+	return c;
+=======
+
+int func_add(int a, int b)
+{
+	retunr a+b;
+>>>>>>> branch1
+}	
